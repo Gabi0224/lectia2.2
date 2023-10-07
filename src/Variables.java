d7975ebb89767d8e8e725ad5502bc2a8c5574fe9
gabi <botezatug65@gmail.com>
2023-10-07 14:25:08 +0300

    lectia2

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] ars) {
+        String g = "gabi";
+        int a = 3;
+        double f = 2.4;
+        System.out.println(g);
+        System.out.println(a);
+        System.out.println(f);
+
+
+    }
+}
